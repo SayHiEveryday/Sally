37188041a57339dbc206586747092b428ec9351d
SayHi <103105268+SayHiEveryday@users.noreply.github.com>
2023-11-21 20:25:26 +0700

    fix bug

--- src/commands/misc/role.py
+++ src/commands/misc/role.py
@@ -6,9 +6,12 @@ class role(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @nextcord.slash_command("role" , default_member_permissions=268435456)
+    async def role_slash(self,interaction: nextcord.Interaction):
+        pass
 
-    @nextcord.slash_command(name="role-all" , description="add a role to all the member in the guild" , default_member_permissions=268435456)
-    async def role_slash_all(interaction: nextcord.Interaction , role: nextcord.Role):
+    @role_slash.subcommand(name="all" , description="add a role to all the member in the guild")
+    async def role_slash_all(self,interaction: nextcord.Interaction , role: nextcord.Role):
         try:
             guild = interaction.guild
             message = await interaction.response.send_message(embed=nextcord.Embed(description=f"**Status:** adding role to {guild.member_count} Will take around {guild.member_count} minutes", colour=nextcord.Color.blurple()))
@@ -25,8 +28,8 @@ class role(commands.Cog):
         else:
             return
         
-    @nextcord.slash_command(name="role-other" , description="add a role to all the member in the guild" , default_member_permissions=268435456)
-    async def role_slash_other(interaction: nextcord.Interaction , role: nextcord.Role):
+    @role_slash.subcommand(name="other" , description="add a role to all the member in the guild")
+    async def role_slash_other(self,interaction: nextcord.Interaction , role: nextcord.Role):
         try:
             guild = interaction.guild
             message = await interaction.response.send_message(embed=nextcord.Embed(description=f"**Status:** Adding role to {guild.member_count - 1} \nWill take around {guild.member_count - 1} minutes", colour=nextcord.Color.blurple()))
@@ -45,8 +48,8 @@ class role(commands.Cog):
         else:
             return
 
-    @nextcord.slash_command(name="role-add" , description="add role to mentioned member" , default_member_permissions=268435456)
-    async def role_slash_add(interaction: nextcord.Interaction , member: nextcord.Member , role: nextcord.Role):
+    @role_slash.subcommand(name="add" , description="add role to mentioned member")
+    async def role_slash_add(self,interaction: nextcord.Interaction , member: nextcord.Member , role: nextcord.Role):
         try:
             if role in member:
                 await interaction.response.send_message(f"{member.mention} already have {role.mention}" , ephemeral=True)
@@ -59,8 +62,8 @@ class role(commands.Cog):
         else:
             return
         
-    @nextcord.slash_command(name="role-remove" , description="remove role to mentioned member" , default_member_permissions=268435456)
-    async def role_slash_remove(interaction: nextcord.Interaction , member: nextcord.Member , role: nextcord.Role):
+    @role_slash.subcommand(name="remove" , description="add role to mentioned member")
+    async def role_slash_remove(self,interaction: nextcord.Interaction , member: nextcord.Member , role: nextcord.Role):
         try:
             if not role in member:
                 await interaction.response.send_message(f"{member.mention} doesn't have {role.mention}" , ephemeral=True)
@@ -75,7 +78,7 @@ class role(commands.Cog):
         
     @commands.command("role")
     @commands.has_permissions(manage_roles=True)
-    async def role_prefix(ctx:commands.Context , member:nextcord.Member , role: nextcord.Role):
+    async def role_prefix(self,ctx:commands.Context , member:nextcord.Member , role: nextcord.Role):
         try:
             if role in member:
                 await member.remove_roles(roles=role , reason=f"Removed by {ctx.author.name}")
@@ -90,4 +93,4 @@ class role(commands.Cog):
             return
     
 def setup(bot):
-    bot.addcog(role(bot))
+    bot.add_cog(role(bot))
