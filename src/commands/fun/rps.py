41d1874363da18ea6eb7a05fb4c635f7aed2411f
SayHi <103105268+SayHiEveryday@users.noreply.github.com>
2023-11-21 20:36:10 +0700

    Update rps.py

--- src/commands/fun/rps.py
+++ src/commands/fun/rps.py
@@ -2,7 +2,7 @@ import nextcord
 from nextcord.ext import commands
 from random import randint
 
-rps: list[str] = ["rock" , "paper" , "scissor"]
+rps_choice = ["rock" , "paper" , "scissor"]
 
 class rps(commands.Cog):
     def __init__(self, client):
@@ -10,7 +10,7 @@ class rps(commands.Cog):
 
     @nextcord.slash_command(name="rps" , description="Play Rock Paper Scissor with bot")
     async def rps_slash(self,interaction: nextcord.Interaction,choice=nextcord.SlashOption(name="choice",description="Your choice",choices=["rock" , "paper" , "scissor"],required=True)):
-        randomed = rps[randint(0,2)]
+        randomed = rps_choice[randint(0,2)]
         if randomed == "rock" and choice == "rock":
             w = "Tie"
         elif randomed == "rock" and choice == "paper":
@@ -31,7 +31,7 @@ class rps(commands.Cog):
             w = "Tie"
 
         embed = nextcord.Embed(
-            description=f"Your choice: {choice}\nMy choice: {randomed}\nResult: {w}",
+            description=f"**Your choice: {choice}**\n**My choice: {randomed}**\n**Result: {w}**",
             colour=nextcord.Color.blurple()
         )
         await interaction.response.send_message(embed=embed)
