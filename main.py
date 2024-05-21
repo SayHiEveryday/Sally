6c35a16e333c1f499728cd7afe7fbbaa8bb81894
SayHiEveryday <piyaphumyt@gmail.com>
2024-05-21 12:53:34 +0700

    e

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from commands.bot import botcommandbp
 
 app = Quart(__name__)
 discord = DiscordInteractions(app)
+discord.update_commands()
 
 discord.register_blueprint(botcommandbp)
 
@@ -15,7 +16,7 @@ app.config["DISCORD_CLIENT_SECRET"] = constant.secret
 app.config["DISCORD_PUBLIC_KEY"] = constant.publickey
 
 discord.set_route_async("/interactions")
-discord.update_commands()
+discord.verify_signature()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=int(os.environ['PORT']))
