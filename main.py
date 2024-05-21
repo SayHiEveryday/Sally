1e439208ae7dc15990021b8b15ddb24abc1fa6ff
SayHiEveryday <piyaphumyt@gmail.com>
2024-05-21 12:34:25 +0700

    e

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ discord.set_route_async("/interactions")
 discord.update_commands()
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ['PORT']))
+    app.run(host="0.0.0.0",port=int(os.environ['PORT']))
